9a311cb22cbdd5059f442f04349aa14fc44d1976
shimon <shimon007@hotmail.com>
2018-11-25 22:23:49 +0200

    התחלת אפליקציה 2 1.התחלת לוגין נותר לממש את הבקנד(יש לראות ששומרים hash ולא את הסיסמא בשרת) 2.מעבר לאקטיביטי אחר לנהגים מצטרפים נדאר לממש הכנסת פרטים ,

---
+++ app/src/main/java/com/project/secondapp/controller/AddDriver.java
@@ -0,0 +1,17 @@
+package com.project.secondapp.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.secondapp.R;
+
+public class AddDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_driver);
+
+
+    }
+}
--- app/src/main/java/com/project/secondapp/controller/MainActivity.java
+++ app/src/main/java/com/project/secondapp/controller/MainActivity.java
@@ -1,5 +1,7 @@
 package com.project.secondapp.controller;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,44 +9,61 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.project.secondapp.R;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.spec.SecretKeySpec;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     //region init
     String pass;
     String name;
     EditText userName;
     EditText password;
+    Button newDriver;
     Button enterUser;
+
     //endregion
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userName=(EditText) findViewById(R.id.user_name);
-        password=(EditText) findViewById(R.id.password);
-        enterUser=(Button) findViewById(R.id.signtbtn);
-        enterUser.setOnClickListener(this);
+        userName = (EditText) findViewById(R.id.user_name);
+        password = (EditText) findViewById(R.id.password);
+        enterUser = (Button) findViewById(R.id.signtbtn);
+        newDriver = (Button) findViewById(R.id.ndriver);
+        newDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent addDriver = new Intent(MainActivity.this, AddDriver.class);
+                 String DriverName=enterUser.getText().toString();
+                addDriver.putExtra("DriverName",DriverName);
+                startActivity(addDriver);
+            }
+        });
+        enterUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (userName.getText().toString() != "") {
+                        name = userName.getText().toString();
+                        pass = enterUser();
+                        //TODO GO TO DB
+                    }
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
     public String enterUser() throws NoSuchAlgorithmException {
         MessageDigest digest = java.security.MessageDigest
-                .getInstance("MD5");digest.update(password.getText().toString().getBytes());
+                .getInstance("MD5");
+        digest.update(password.getText().toString().getBytes());
         return digest.digest().toString();
     }
 
-    @Override
-    public void onClick(View v) {
-        try {
-         if(userName.getText().toString()!="") {
-             name=userName.getText().toString();
-             pass = enterUser();
-             //TODO GO TO DB
-         }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
 }
